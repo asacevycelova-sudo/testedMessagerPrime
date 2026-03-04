e7299d7c9fa9f8e29f15187f0a80c32e9d013c8a
Pavel <asacevycelova@gmail.com>
2026-03-04 13:32:50 +0300

    result

--- mainPrime.py
+++ mainPrime.py
@@ -8,7 +8,7 @@ class User():
         self.name = input("Enter name: ")
         self.age = int(input("Enter age: "))
         if self.age == "root" and self.age >= 18:
-            print("Добро пожаловать на сервер БЕЗУМНЫЕ ЗОМБИ")
+            print("Добро пожаловать на сервер БЕЗУМНЫЕ ЗОМБИ\nБЕСПЛАТНАЯ ВИПКА\nДОНАТ ЧЕРЕЗ ВК")
 
 def main():
     users = User(" ", 0)
